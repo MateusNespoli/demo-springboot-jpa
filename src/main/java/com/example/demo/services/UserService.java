f94a1c7b50b5b15d2c6210ce1c278baf4cc8e19b
MateusNespoli <141879762+MateusNespoli@users.noreply.github.com>
2023-12-09 14:13:12 -0300

    User delete

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.User;
@@ -26,5 +27,10 @@ public class UserService {
 
     public User insert(User obj) {
         return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+        
     }
 }
